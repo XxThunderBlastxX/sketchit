2790a16a66162a68b36eb87616012e010d621cdb
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-06-26 03:08:18 +0530

    Refactored

--- sketchit_flutter/lib/src/feature/sketch_canvas/presentation/screens/canvas_screen.dart
+++ sketchit_flutter/lib/src/feature/sketch_canvas/presentation/screens/canvas_screen.dart
@@ -5,8 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../app/theme/theme.dart';
 import '../bloc/sketch_bloc/sketch_bloc.dart';
 import '../bloc/sketch_menu_bar_bloc/sketch_menu_bar_bloc.dart';
-import '../model/sketch_stroke.dart';
-import 'sketch_painter.dart';
+import 'widgets/drawing_canvas.dart';
 import 'widgets/sketch_menu_bar.dart';
 
 class CanvasScreen extends StatelessWidget {
@@ -54,104 +53,6 @@ class CanvasScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class DrawingCanvas extends StatelessWidget {
-  const DrawingCanvas({super.key});
-
-  void _onPointerDown(
-    PointerDownEvent details,
-    BuildContext context,
-    SketchMenuBarState menuBarState,
-  ) {
-    final transformedOffset = context
-        .read<SketchBloc>()
-        .transformationController
-        .toScene(details.position);
-
-    final sketchStroke = SketchStroke(
-      color: menuBarState.sketchMode == SketchMode.erase
-          ? menuBarState.eraserColor
-          : menuBarState.strokeColor,
-      strokeWidth: 2.0,
-      offsetList: [transformedOffset],
-      sketchMode: menuBarState.sketchMode,
-    );
-    context.read<SketchBloc>().add(StartSketch(sketchStroke));
-  }
-
-  void _onPointerMove(
-    PointerMoveEvent details,
-    BuildContext context,
-    SketchMenuBarState menuBarState,
-    SketchState state,
-  ) {
-    final transformedOffset = context
-        .read<SketchBloc>()
-        .transformationController
-        .toScene(details.position);
-
-    var sketchStroke = SketchStroke(
-      color: menuBarState.sketchMode == SketchMode.erase
-          ? menuBarState.eraserColor
-          : menuBarState.strokeColor,
-      strokeWidth: 2.0,
-      offsetList: List.from(state.currentSketchStroke.offsetList)
-        ..add(transformedOffset),
-      sketchMode: menuBarState.sketchMode,
-    );
-    context.read<SketchBloc>().add(ContinueSketch(sketchStroke));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<SketchMenuBarBloc, SketchMenuBarState>(
-      builder: (context, menuBarState) {
-        return BlocBuilder<SketchBloc, SketchState>(
-          builder: (context, sketchState) {
-            return RepaintBoundary(
-              child: Listener(
-                onPointerDown: (details) => _onPointerDown(
-                  details,
-                  context,
-                  menuBarState,
-                ),
-                onPointerMove: (details) => _onPointerMove(
-                  details,
-                  context,
-                  menuBarState,
-                  sketchState,
-                ),
-                onPointerUp: (_) => context.read<SketchBloc>().add(EndSketch()),
-                child: MouseRegion(
-                  cursor: menuBarState.sketchMode.systemCursor,
-                  onHover: (enter) {},
-                  child: InteractiveViewer(
-                    minScale: 0.1,
-                    maxScale: 3.0,
-                    panEnabled: menuBarState.sketchMode == SketchMode.pan,
-                    transformationController:
-                        context.read<SketchBloc>().transformationController,
-                    child: CustomPaint(
-                      painter: SketchPainter(
-                        List.from(sketchState.sketchStrokes)
-                          ..add(sketchState.currentSketchStroke),
-                        context
-                            .read<SketchBloc>()
-                            .transformationController
-                            .value,
-                      ),
-                      child: const SizedBox.expand(),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
-        );
-      },
     );
   }
 }
