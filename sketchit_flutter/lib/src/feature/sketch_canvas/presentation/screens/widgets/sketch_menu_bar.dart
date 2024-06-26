7b5db05d104df2a38c51bbb83622eeb6dfb2a921
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-06-26 18:53:05 +0530

    Responsive height of the menue bar

--- sketchit_flutter/lib/src/feature/sketch_canvas/presentation/screens/widgets/sketch_menu_bar.dart
+++ sketchit_flutter/lib/src/feature/sketch_canvas/presentation/screens/widgets/sketch_menu_bar.dart
@@ -19,121 +19,121 @@ class SketchMenuBar extends StatelessWidget {
           padding: const EdgeInsets.all(18.0),
           child: Align(
             alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              height: 50.0,
-              // width: 520.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 2,
-                    offset: const Offset(0, 1),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.draw)),
-                    tooltip: 'Pen',
-                    color: state.sketchMode == SketchMode.draw
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  IconButton(
-                    icon: const Icon(IconData(
-                      59648,
-                      fontFamily: 'icomoon',
-                    )),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.erase)),
-                    tooltip: 'Eraser',
-                    color: state.sketchMode == SketchMode.erase
-                        ? Colors.blue
-                        : Colors.black.withOpacity(0.75),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.pan_tool_rounded),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.pan)),
-                    tooltip: 'Move',
-                    color: state.sketchMode == SketchMode.pan
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  const VerticalDivider(),
-                  SketchMenuBarColors(),
-                  const VerticalDivider(),
-                  IconButton(
-                    icon: const Icon(Icons.minimize_outlined),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.line)),
-                    tooltip: 'Line',
-                    color: state.sketchMode == SketchMode.line
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.circle_outlined),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.circle)),
-                    tooltip: 'Circle',
-                    color: state.sketchMode == SketchMode.circle
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.rectangle_outlined),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.rectangle)),
-                    tooltip: 'Rectangle',
-                    color: state.sketchMode == SketchMode.rectangle
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.hexagon_outlined),
-                    onPressed: () => context
-                        .read<SketchMenuBarBloc>()
-                        .add(const ChangeSketchMode(SketchMode.hexagon)),
-                    tooltip: 'Hexagon',
-                    color: state.sketchMode == SketchMode.hexagon
-                        ? Colors.blue
-                        : Colors.black,
-                  ),
-                  const VerticalDivider(),
-                  IconButton(
-                    icon: const Icon(Icons.undo),
-                    onPressed: () => sketchBloc.add(UndoSketch()),
-                    tooltip: 'Undo',
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.redo),
-                    onPressed: () => sketchBloc.add(RedoSketch()),
-                    tooltip: 'Redo',
-                  ),
-                  const VerticalDivider(),
-                  IconButton(
-                    icon: const Icon(Icons.delete_rounded),
-                    onPressed: () =>
-                        context.read<SketchBloc>().add(ClearSketch()),
-                    tooltip: 'Clear',
-                  ),
-                ],
+            child: IntrinsicHeight(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: const Offset(0, 1),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.draw)),
+                      tooltip: 'Pen',
+                      color: state.sketchMode == SketchMode.draw
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(IconData(
+                        59648,
+                        fontFamily: 'icomoon',
+                      )),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.erase)),
+                      tooltip: 'Eraser',
+                      color: state.sketchMode == SketchMode.erase
+                          ? Colors.blue
+                          : Colors.black.withOpacity(0.75),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.pan_tool_rounded),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.pan)),
+                      tooltip: 'Move',
+                      color: state.sketchMode == SketchMode.pan
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    const VerticalDivider(),
+                    SketchMenuBarColors(),
+                    const VerticalDivider(),
+                    IconButton(
+                      icon: const Icon(Icons.minimize_outlined),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.line)),
+                      tooltip: 'Line',
+                      color: state.sketchMode == SketchMode.line
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.circle_outlined),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.circle)),
+                      tooltip: 'Circle',
+                      color: state.sketchMode == SketchMode.circle
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.rectangle_outlined),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.rectangle)),
+                      tooltip: 'Rectangle',
+                      color: state.sketchMode == SketchMode.rectangle
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.hexagon_outlined),
+                      onPressed: () => context
+                          .read<SketchMenuBarBloc>()
+                          .add(const ChangeSketchMode(SketchMode.hexagon)),
+                      tooltip: 'Hexagon',
+                      color: state.sketchMode == SketchMode.hexagon
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                    const VerticalDivider(),
+                    IconButton(
+                      icon: const Icon(Icons.undo),
+                      onPressed: () => sketchBloc.add(UndoSketch()),
+                      tooltip: 'Undo',
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.redo),
+                      onPressed: () => sketchBloc.add(RedoSketch()),
+                      tooltip: 'Redo',
+                    ),
+                    const VerticalDivider(),
+                    IconButton(
+                      icon: const Icon(Icons.delete_rounded),
+                      onPressed: () =>
+                          context.read<SketchBloc>().add(ClearSketch()),
+                      tooltip: 'Clear',
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
